8e737e2af54969d4bc0d57fd23d1cdb345591da0
Kemal Hasan Atay <kha26@cornell.edu>
2018-07-22 15:41:26 +0300

    Initial release

--- example.js
+++
@@ -1,9 +0,0 @@
-const Query = require('./');
-
-        let queryString = Query()
-            .insert('user')
-            .defaultExpression('createdAt', 'NOW()')
-            .whereEqSt('name', 'George')
-            .query();
-
-console.log(queryString);
